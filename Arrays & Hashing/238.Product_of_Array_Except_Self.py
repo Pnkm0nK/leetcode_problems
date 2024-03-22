fa436c634560bdf86926053caf4cfb5f4489e5e9
Mikita Zyhmantovich <mikita.zyhmantovich@stud.th-deg.de>
2024-03-22 23:19:55 +0100

    Add 238. solution

--- Arrays & Hashing/238.Product_of_Array_Except_Self.py
+++ Arrays & Hashing/238.Product_of_Array_Except_Self.py
@@ -1,7 +1,6 @@
 nums = [1,2,3,4]
 n = len(nums)
 suffixes = [None for _ in range(len(nums))]
-prefixes = [None for _ in range(len(nums))]
 suffixes[-1] = 1
 
 for i in range(n - 2, -1, -1):
@@ -9,10 +8,10 @@ for i in range(n - 2, -1, -1):
 
 for i in range(n):
     if i - 1 < 0:
-        prefixes[i] = 1
+        pref = 1
     else:
-        prefixes[i] = nums[i-1] * prefixes[i-1]
-    suffixes[i] = prefixes[i] * suffixes[i]
+        pref = nums[i-1] * pref
+    suffixes[i] = pref * suffixes[i]
 
 print(suffixes)
 
